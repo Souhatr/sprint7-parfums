059c7e7b5e9f4753515ba1ea1de6b4bf1fac077f
unknown <souhatroudi04@gmail.com>
2025-04-09 22:44:12 +0100

    first commit

---
+++ src/main/java/com/souha/parfums/entities/ParfumProjection.java
@@ -0,0 +1,7 @@
+package com.souha.parfums.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+@Projection(name = "nomParf", types = { Parfum.class })
+public interface ParfumProjection {
+	public String getNomParfum();
+}
